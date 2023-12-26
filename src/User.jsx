d6f1260dfdbbf12048be53fab3240e058f9052d2
Clintoy17 <clintmagalona@gmail.com>
2023-12-26 09:59:07 +0800

    Initial commit
    Generated by create-expo-app 2.1.1.

---
+++ src/User.jsx
@@ -0,0 +1,83 @@
+export const users = [
+    {
+      id: 1,
+      name: {
+        firstname: "JOHNBHERT CLINT",
+        middlename: "P.",
+        lastname: "MAGALONA",
+      },
+      address: "Lourdes, Bohol",
+    },
+    {
+      id: 2,
+      name: {
+        firstname: "MIRADEL",
+        middlename: "S.",
+        lastname: "BAMUYA",
+      },
+      address: "Tiptip, Tagbilaran City",
+    },
+    {
+      id: 3,
+      name: {
+        firstname: "SHANE",
+        middlename: "P.",
+        lastname: "MAGALONA",
+      },
+      address: "Lourdes, Bohol",
+    },
+    {
+      id: 4,
+      name: {
+        firstname: "VALERIANA",
+        middlename: "B.",
+        lastname: "POCOT",
+      },
+      address: "Lourdes, Magalona",
+    },
+    {
+      id: 5,
+      name: {
+        firstname: "JHEMART",
+        middlename: "T.",
+        lastname: "POCOT",
+      },
+      address: "Lourdes, Bohol",
+    },
+    {
+      id: 6,
+      name: {
+        firstname: "GERARD",
+        middlename: "",
+        lastname: "GENCIANOS",
+      },
+      address: "Ubujan, Tagbilaran City",
+    },
+    {
+      id: 7,
+      name: {
+        firstname: "DRANREB DJ",
+        middlename: "J.",
+        lastname: "PARING",
+      },
+      address: "Lourdes, Bohol",
+    },
+    {
+      id: 8,
+      name: {
+        firstname: "MARK GIDEON",
+        middlename: "",
+        lastname: "FUDOLIG",
+      },
+      address: "Salvador, Bohol",
+    },
+    {
+      id: 9,
+      name: {
+        firstname: "CLARENCE JHON",
+        middlename: "",
+        lastname: "QUIJANO",
+      },
+      address: "Hanopol, Balilihan",
+    },
+  ];
